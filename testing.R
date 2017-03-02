997af7a75ee95b3ca5883300b56f3a2561d8ff72
jklen <klen.jaro@gmail.com>
2017-03-02 16:23:46 +0100

    rows to keep

--- testing.R
+++ testing.R
@@ -5,9 +5,52 @@ library(stringr)
 
 tesseract_info()
 engine <- tesseract('slk')
-path_receipts <- '/home/user/Scanned stuff/Receipts/'
+path_receipts <- '/home/user/Scanned stuff/Receipts/Billa'
 path_receipts2 <- 'C:/Users/IBM_ADMIN/Desktop/MY STUFF/Billa'
 shops <- c('Billa')
+
+ocr_receipt_list <- str_split(ocr(image = paste(path_receipts, '/', list.files(path = path_receipts), sep = ''), 
+                         engine = engine), '\n')
+
+# pre kazdy vektor v liste, necham:
+#   - 5. prevadzka
+#   - 6. datum a cas
+#   - 8 az po prvy vyskyt DPH -1, alebo REKAPITULACIA -1, alebo SADZBA -2
+#   - tam kde je prvy vyskyt SUCET, alebo EUR, ciastku pre verifikaciu
+
+remove_un <- function(x){
+  
+ index_to_keep <- 5
+ last_item_index <- grep('^([0|D|O]PH) ([[:alnum:]]{13})$', x) - 1 # index riadku poslednej polozky
+ print(last_item_index)
+ 
+ # adresa prevadzky, datum a cas
+ 
+ if (str_detect(x[5], '\\d{1,2},\\d{5}')){
+   
+   # ak je adresa rozdelena ciarkou v jednom riadku
+   
+   index_to_keep[2] <-  6
+   index_to_keep[3:(last_item_index - 5)] <- 8:last_item_index 
+   
+   
+ } else {
+   
+   # ak je adresa v dvoch riadkoch
+   
+   index_to_keep[2] <- 6
+   index_to_keep[3] <- 7
+   
+ }
+ 
+ # polozky
+ 
+ 
+ 
+ x <- x[index_to_keep]
+
+}
+
 
 
 # potrebujem
